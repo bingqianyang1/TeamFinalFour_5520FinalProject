e736cdfcc9e81fe35a6740ed1470f25b2937a3b7
Jinyin Chai <cjy990324>
2022-04-08 14:26:14 -0700

    set up firebase connection

--- app/src/main/java/edu/neu/madcourse/finalproject/AddPostActivity.java
+++ app/src/main/java/edu/neu/madcourse/finalproject/AddPostActivity.java
@@ -2,7 +2,14 @@ package edu.neu.madcourse.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class AddPostActivity extends AppCompatActivity {
 
@@ -10,5 +17,24 @@ public class AddPostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
+        Button select_image_button = findViewById(R.id.choose_image_button);
+        select_image_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 3);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null){
+            Uri selected_image = data.getData();
+            ImageView imageView = findViewById(R.id.add_image);
+            imageView.setImageURI(selected_image);
+        }
     }
 }
--- app/src/main/java/edu/neu/madcourse/finalproject/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/finalproject/MainActivity.java
@@ -2,13 +2,28 @@ package edu.neu.madcourse.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button add_post_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add_post_button = findViewById(R.id.add_post_button);
+        add_post_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                Intent intent = new Intent(MainActivity.this, AddPostActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
