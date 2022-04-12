af1facc383501418d0aa22176639194770e3af1b
zengxuan <zeng.xu@northeastern.edu>
2022-04-12 00:26:31 -0700

    implemented show follow activity

--- app/src/main/java/edu/neu/madcourse/finalproject/ShowFollowActivity.java
+++ app/src/main/java/edu/neu/madcourse/finalproject/ShowFollowActivity.java
@@ -1,14 +1,57 @@
 package edu.neu.madcourse.finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class ShowFollowActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference reference;
+    private FollowAdapter adapter;
+    private ArrayList<FollowModel> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_follow);
+
+
+        recyclerView = findViewById(R.id.followReView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list=new ArrayList<>();
+        adapter = new FollowAdapter(this,list);
+        recyclerView.setAdapter(adapter);
+
+        String username = getIntent().getStringExtra("username");
+        reference = db.getReference().child("Users").child(username).child("following");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    FollowModel model = dataSnapshot.getValue(FollowModel.class);
+                    list.add(model);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
     }
 }
