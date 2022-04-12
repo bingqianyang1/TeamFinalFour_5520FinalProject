b82c7d7bc9aa87e785723f3b41487fec858f7565
zengxuan <zeng.xu@northeastern.edu>
2022-04-12 10:13:46 -0700

    updated model and adapter

--- app/src/main/java/edu/neu/madcourse/finalproject/FollowAdapter.java
+++ app/src/main/java/edu/neu/madcourse/finalproject/FollowAdapter.java
@@ -1,2 +1,49 @@
-package edu.neu.madcourse.finalproject;public class FollowAdapter {
+package edu.neu.madcourse.finalproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class FollowAdapter extends RecyclerView.Adapter<FollowAdapter.MyViewHolder>{
+    ArrayList<FollowModel> list;
+    Context context;
+
+    public FollowAdapter(Context context, ArrayList<FollowModel>list){
+        this.list=list;
+        this.context=context;
+    }
+
+    @NonNull
+    @Override
+    public FollowAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.follow_item, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FollowAdapter.MyViewHolder holder, int position) {
+        FollowModel model = list.get(position);
+        holder.followText.setText(model.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView followText;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            followText = itemView.findViewById(R.id.followText);
+        }
+    }
 }
--- app/src/main/java/edu/neu/madcourse/finalproject/FollowModel.java
+++ app/src/main/java/edu/neu/madcourse/finalproject/FollowModel.java
@@ -1,2 +1,8 @@
-package edu.neu.madcourse.finalproject;public class FollowModel {
+package edu.neu.madcourse.finalproject;
+
+public class FollowModel {
+    private String username;
+    public String getName(){
+        return this.username;
+    }
 }
